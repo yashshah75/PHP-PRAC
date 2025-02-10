9d1c61d4eb2406e571c6ee1fbc563bdf8aafc354
yashshah75 <yashshah29034@gmail.com>
2025-02-11 00:00:40 +0530

    File Upload

--- 4. Task_1_ login_register/db.php
+++ 4. Task_1_ login_register/db.php
@@ -5,7 +5,7 @@ $password = "password";
 $dbname = "project";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
-
+// SQL CONNECTION CHECK
 if (!$conn) 
 {
   die("Connection failed: " . mysqli_connect_error());
