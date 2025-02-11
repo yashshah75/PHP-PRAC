22014c5eb3560817325fcaf6c8e62fb2b04f5bdf
yashshah75 <yashshah29034@gmail.com>
2025-02-11 14:21:25 +0530

    File Upload

--- 4. Task_1_ login_register/db.php
+++ 4. Task_1_ login_register/db.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
-$username = "username";
-$password = "password";
+$username = "root";
+$password = "";
 $dbname = "project";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
