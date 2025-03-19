44e8cf6545229d58471277c6ed95c58233c416d4
Yash Shah <yashshah29034@gmail.com.github.com>
2025-03-19 18:53:27 +0530

    Initial commit

--- Mantis-Bootstrap/dashboard/database/db.php
+++ Mantis-Bootstrap/dashboard/database/db.php
@@ -2,6 +2,17 @@
     $servername = "localhost";
     $username = "root";
     $password = "";
-    
+    $dbname = "crud";
 
-?>
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+    if($conn)
+    {
+        // header('location:sidebar.php');
+        echo "Connected";
+    }
+    else
+    {
+        echo "disconnected";
+    }
+    ?>
--- Mantis-Bootstrap/dashboard/index.php
+++ Mantis-Bootstrap/dashboard/index.php
@@ -1,4 +1,5 @@
 <?php 
+include('database/db.php');
 
 include('sidebar.php');
 include('header.php');
