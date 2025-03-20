ae39418f467e41e62013a96bc3152be1f18d17bb
Yash Shah <yashshah29034@gmail.com.github.com>
2025-03-20 12:59:50 +0530

    Initial commit

--- Mantis-Bootstrap/dashboard/index.php
+++ Mantis-Bootstrap/dashboard/index.php
@@ -1,62 +1,64 @@
-<?php 
-include('database/db.php');
 
+<?php
+include('database/db.php');
 include('sidebar.php');
 include('header.php');
-// include('main.php');
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
-<!-- [Head] start -->
-
-
-<!-- Mirrored from themewagon.github.io/Mantis-Bootstrap/dashboard/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 18 Mar 2025 13:10:11 GMT -->
-<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=utf-8" /><!-- /Added by HTTrack -->
-<head>
-  <title>Home | Mantis Bootstrap 5 Admin Template</title>
-  <!-- [Meta] -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="description" content="Mantis is made using Bootstrap 5 design framework. Download the free admin template & use it for your project.">
+  <!-- [Head] start -->
+  
+  
+  <!-- Mirrored from themewagon.github.io/Mantis-Bootstrap/dashboard/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 18 Mar 2025 13:10:11 GMT -->
+  <!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=utf-8" /><!-- /Added by HTTrack -->
+  <head>
+    <title>Home | Mantis Bootstrap 5 Admin Template</title>
+    <!-- [Meta] -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="Mantis is made using Bootstrap 5 design framework. Download the free admin template & use it for your project.">
   <meta name="keywords" content="Mantis, Dashboard UI Kit, Bootstrap 5, Admin Template, Admin Dashboard, CRM, CMS, Bootstrap Admin Template">
   <meta name="author" content="CodedThemes">
-
+  
   <!-- [Favicon] icon -->
   <link rel="icon" href="https://themewagon.github.io/Mantis-Bootstrap/assets/images/favicon.svg" type="image/x-icon"> <!-- [Google Font] Family -->
-<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Public+Sans:wght@300;400;500;600;700&amp;display=swap" id="main-font-link">
-<!-- [Tabler Icons] https://tablericons.com -->
-<link rel="stylesheet" href="../assets/fonts/tabler-icons.min.css" >
-<!-- [Feather Icons] https://feathericons.com -->
-<link rel="stylesheet" href="../assets/fonts/feather.css" >
-<!-- [Font Awesome Icons] https://fontawesome.com/icons -->
-<link rel="stylesheet" href="../assets/fonts/fontawesome.css" >
-<!-- [Material Icons] https://fonts.google.com/icons -->
-<link rel="stylesheet" href="../assets/fonts/material.css" >
-<!-- [Template CSS Files] -->
-<link rel="stylesheet" href="../assets/css/style.css" id="main-style-link" >
-<link rel="stylesheet" href="../assets/css/style-preset.css" >
-
-
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Public+Sans:wght@300;400;500;600;700&amp;display=swap" id="main-font-link">
+  <!-- [Tabler Icons] https://tablericons.com -->
+  <link rel="stylesheet" href="../assets/fonts/tabler-icons.min.css" >
+  <!-- [Feather Icons] https://feathericons.com -->
+  <link rel="stylesheet" href="../assets/fonts/feather.css" >
+  <!-- [Font Awesome Icons] https://fontawesome.com/icons -->
+  <link rel="stylesheet" href="../assets/fonts/fontawesome.css" >
+  <!-- [Material Icons] https://fonts.google.com/icons -->
+  <link rel="stylesheet" href="../assets/fonts/material.css" >
+  <!-- [Template CSS Files] -->
+  <link rel="stylesheet" href="../assets/css/style.css" id="main-style-link" >
+  <link rel="stylesheet" href="../assets/css/style-preset.css" >
+  
 </head>
 <!-- [Head] end -->
 <!-- [Body] Start -->
-
 <body data-pc-preset="preset-1" data-pc-direction="ltr" data-pc-theme="light">
+  
   <!-- [ Pre-loader ] start -->
-<div class  ="loader-bg">
-  <div class="loader-track">
-    <div class="loader-fill"></div>
+  <div class="loader-bg">
+    <div class="loader-track">
+      <div class="loader-fill"></div>
+    </div>
   </div>
-</div>
-<!-- [ Pre-loader ] End -->
 
 
- <!-- [ Main Content ] start -->
- <div class="pc-container">
+
+  <!-- [ Pre-loader ] End -->
+  <!-- [ Sidebar Menu ] end --> <!-- [ Header Topbar ] start -->
+  <!-- [ Main Content ] start -->
+  <div class="pc-container">
     <div class="pc-content">
-      <!-- [ breadcrumb ] start -->
-      <div class="page-header">
+      <!-- [ Main Content ] start -->
+     
+<div class="page-header">
         <div class="page-block">
           <div class="row align-items-center">
             <div class="col-md-12">
@@ -72,19 +74,70 @@ include('header.php');
           </div>
         </div>
       </div>
-      <!-- [ breadcrumb ] end -->
-      <!-- [ Main Content ] start -->
+
+
+      <!-- =============================================================================================== -->
+                                        <!-- CRUD TABLE  -->
+      <!-- =============================================================================================== -->
+      <table class="table table-bordered">
+      <thead>
+        <tr>
+          <th >ID</th>
+          <th>USER NAME</th>
+          <th>EmEMAILail</th>
+          <th>PASSWORD</th>
+          <th>CONFIRM_PASSWORD</th>
+          <th>MOBILE</th>
+          <th>ACTIONS</th>
+        </tr>
+      </thead>
+      <tbody>
+       
+        <tr>
+          <th scope="row">1</th>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+          <td>@mdo</td>
+          <td>@mdo</td>
+          <td>
+              <button type="button" class="btn btn-success">UPDATE</button> &nbsp;
+              <button type="button" class="btn btn-danger">DELETE</button>
+          </td>
+        </tr>
+      
+        <tr>
+          <th>2</th>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+          <td>@mdo</td>
+          <td>@mdo</td>
+          <td>
+              <button type="button" class="btn btn-success">UPDATE</button> &nbsp;
+              <button type="button" class="btn btn-danger">DELETE</button>
+          </td>
+        </tr>
+      
+        <tr>
+          <th>3</th>
+          <td>Larry the Bird</td>
+          <td>@twitter</td>
+          <td>@fat</td>
+          <td>@mdo</td>
+          <td>@mdo</td>
+          <td>
+              <button type="button" class="btn btn-success">UPDATE</button> &nbsp;
+              <button type="button" class="btn btn-danger">DELETE</button>
+          </td>
+        </tr>
+        
+      </tbody>
+    </table>
     </div>
   </div>
   <!-- [ Main Content ] end -->
- 
-
-
-
-
- 
   
-
   <!-- [Page Specific JS] start -->
   <script src="../assets/js/plugins/apexcharts.min.js"></script>
   <script src="../assets/js/pages/dashboard-default.js"></script>
@@ -96,7 +149,7 @@ include('header.php');
   <script src="../assets/js/fonts/custom-font.js"></script>
   <script src="../assets/js/pcoded.js"></script>
   <script src="../assets/js/plugins/feather.min.js"></script>
-
+  
   
   
   
@@ -117,14 +170,12 @@ include('header.php');
   
   
   <script>font_change("Public-Sans");</script>
-  
 
-<?php 
+<?php
 include('footer.php');
 
 ?>
-    
-
+  
 </body>
 <!-- [Body] end -->
 
--- Mantis-Bootstrap/dashboard/sidebar.php
+++ Mantis-Bootstrap/dashboard/sidebar.php
@@ -1,17 +1,13 @@
+
+
  <!-- [ Sidebar Menu ] start -->
  <nav class="pc-sidebar">
   <div class="navbar-wrapper">
     <div class="m-header">
-      <a href="index.php" class="b-brand text-primary">
+      <a href="" class="b-brand text-primary">
         <!-- ========   Change your logo from here   ============ -->
-        <!-- <img src="http://localhostE:\xampp\htdocs\1. AORC TECHNOLOGIES\PRACTICE\7.crud_html\Mantis-Bootstrap\dashboard\assets\images\user\images.png" class="img-fluid logo-lg" alt="logo"> -->
-        <img src=" <?php echo '1.AORC TECHNOLOGIES/PRACTICE/7.crud_html/Mantis-Bootstrap/dashboard/images/user/avatar-1.jpg'; ?>" class="img-fluid logo-lg" alt="logo">
-
-
-
-
-        <!-- ../assets/fonts/feather.css -->
-      </a>
+        <img src="<?php echo '/allfiles/Mantis-Bootstrap/Mantis-Bootstrap/dashboard/imagess/logooo.jpg'; ?>"  alt="logo"  style="width: 50px; height: 50px; display: block !important;">
+        </a>
     </div>
     <div class="navbar-content">
       <ul class="pc-navbar">
@@ -50,74 +46,19 @@
           <i class="ti ti-news"></i>
         </li>
         <li class="pc-item">
-          <a href="https://themewagon.github.io/Mantis-Bootstrap/pages/login.html" class="pc-link">
+          <a href="login.php" class="pc-link">
             <span class="pc-micon"><i class="ti ti-lock"></i></span>
             <span class="pc-mtext">Login</span>
           </a>
         </li>
         <li class="pc-item">
-          <a href="https://themewagon.github.io/Mantis-Bootstrap/pages/register.html" class="pc-link">
+          <a href="register.php" class="pc-link">
             <span class="pc-micon"><i class="ti ti-user-plus"></i></span>
             <span class="pc-mtext">Register</span>
           </a>
         </li>
-
-        <!-- <li class="pc-item pc-caption">
-          <label>Other</label>
-          <i class="ti ti-brand-chrome"></i>
-        </li>
-        <li class="pc-item pc-hasmenu">
-          <a href="#!" class="pc-link"><span class="pc-micon"><i class="ti ti-menu"></i></span><span class="pc-mtext">Menu
-              levels</span><span class="pc-arrow"><i data-feather="chevron-right"></i></span></a>
-          <ul class="pc-submenu">
-            <li class="pc-item"><a class="pc-link" href="#!">Level 2.1</a></li>
-            <li class="pc-item pc-hasmenu">
-              <a href="#!" class="pc-link">Level 2.2<span class="pc-arrow"><i data-feather="chevron-right"></i></span></a>
-              <ul class="pc-submenu">
-                <li class="pc-item"><a class="pc-link" href="#!">Level 3.1</a></li>
-                <li class="pc-item"><a class="pc-link" href="#!">Level 3.2</a></li>
-                <li class="pc-item pc-hasmenu">
-                  <a href="#!" class="pc-link">Level 3.3<span class="pc-arrow"><i data-feather="chevron-right"></i></span></a>
-                  <ul class="pc-submenu">
-                    <li class="pc-item"><a class="pc-link" href="#!">Level 4.1</a></li>
-                    <li class="pc-item"><a class="pc-link" href="#!">Level 4.2</a></li>
-                  </ul>
-                </li>
-              </ul>
-            </li> -->
-            <!-- <li class="pc-item pc-hasmenu">
-              <a href="#!" class="pc-link">Level 2.3<span class="pc-arrow"><i data-feather="chevron-right"></i></span></a>
-              <ul class="pc-submenu">
-                <li class="pc-item"><a class="pc-link" href="#!">Level 3.1</a></li>
-                <li class="pc-item"><a class="pc-link" href="#!">Level 3.2</a></li>
-                <li class="pc-item pc-hasmenu">
-                  <a href="#!" class="pc-link">Level 3.3<span class="pc-arrow"><i data-feather="chevron-right"></i></span></a>
-                  <ul class="pc-submenu">
-                    <li class="pc-item"><a class="pc-link" href="#!">Level 4.1</a></li>
-                    <li class="pc-item"><a class="pc-link" href="#!">Level 4.2</a></li>
-                  </ul>
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </li> -->
-        <!-- <li class="pc-item">
-          <a href="https://themewagon.github.io/Mantis-Bootstrap/other/sample-page.html" class="pc-link">
-            <span class="pc-micon"><i class="ti ti-brand-chrome"></i></span>
-            <span class="pc-mtext">Sample page</span>
-          </a>
-        </li> -->
       </ul>
-    <!-- <div class="card text-center">
-       <div class="card-body">
-          <img src="../assets/images/img-navbar-card.png" alt="images" class="img-fluid mb-2">
-          <h5>Upgrade To Pro</h5>
-          <p>To get more features and components</p>
-          <a href="https://codedthemes.com/item/berry-bootstrap-5-admin-template/" target="_blank"
-          class="btn btn-success">Buy Now</a>
-        </div> 
-      </div>-->
+      
     </div>
   </div>
 </nav>
-<!-- [ Sidebar Menu ] end --> 
