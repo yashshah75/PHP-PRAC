923f8dcb5d41b0ba9fdbb5b45ee6cb6d9fc4a401
Yash Shah <yashshah29034@gmail.com.github.com>
2025-03-26 12:56:18 +0530

    Updated changes

--- Mantis-Bootstrap/dashboard/header.php
+++ Mantis-Bootstrap/dashboard/header.php
@@ -1,4 +1,3 @@
-
 <header class="pc-header">
   <div class="header-wrapper">
     <div class="me-auto pc-mob-drp">
@@ -41,78 +40,13 @@
     <div class="ms-auto">
       <ul class="list-unstyled">
         <li class="dropdown pc-h-item">
-          
           <?php if (isset($_SESSION['user_name'])) {
                 echo "<span>Welcome: ".$_SESSION['user_name']."</span>";}
               else {
                 echo "<span>Guest</span>"; // Default text if the user is not logged in
                     } ?>
-          
-          <a class="pc-head-link dropdown-toggle arrow-none me-0" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
-            <i class="ti ti-mail"></i>
-          </a>
-          <div class="dropdown-menu dropdown-notification dropdown-menu-end pc-h-dropdown">
-            <div class="dropdown-header d-flex align-items-center justify-content-between">
-              <h5 class="m-0">Message</h5>
-              <a href="#!" class="pc-head-link bg-transparent"><i class="ti ti-x text-danger"></i></a>
-            </div>
-            <div class="dropdown-divider"></div>
-            <div class="dropdown-header px-0 text-wrap header-notification-scroll position-relative" style="max-height: calc(100vh - 215px)">
-              <div class="list-group list-group-flush w-100">
-                <a class="list-group-item list-group-item-action">
-                  <div class="d-flex">
-                    <div class="flex-shrink-0">
-                      <img src="../assets/images/user/avatar-2.jpg" alt="user-image" class="user-avtar">
-                    </div>
-                    <div class="flex-grow-1 ms-1">
-                      <span class="float-end text-muted">3:00 AM</span>
-                      <p class="text-body mb-1">It's <b>Cristina danny's</b> birthday today.</p>
-                      <span class="text-muted">2 min ago</span>
-                    </div>
-                  </div>
-                </a>
-                <a class="list-group-item list-group-item-action">
-                  <div class="d-flex">
-                    <div class="flex-shrink-0">
-                      <img src="../assets/images/user/avatar-1.jpg" alt="user-image" class="user-avtar">
-                    </div>
-                    <div class="flex-grow-1 ms-1">
-                      <span class="float-end text-muted">6:00 PM</span>
-                      <p class="text-body mb-1"><b>Aida Burg</b> commented on your post.</p>
-                      <span class="text-muted">5 August</span>
-                    </div>
-                  </div>
-                </a>
-                <a class="list-group-item list-group-item-action">
-                  <div class="d-flex">
-                    <div class="flex-shrink-0">
-                      <img src="../assets/images/user/avatar-3.jpg" alt="user-image" class="user-avtar">
-                    </div>
-                    <div class="flex-grow-1 ms-1">
-                      <span class="float-end text-muted">2:45 PM</span>
-                      <p class="text-body mb-1"><b>There was a failure to your setup.</b></p>
-                      <span class="text-muted">7 hours ago</span>
-                    </div>
-                  </div>
-                </a>
-                <a class="list-group-item list-group-item-action">
-                  <div class="d-flex">
-                    <div class="flex-shrink-0">
-                      <img src="../assets/images/user/avatar-4.jpg" alt="user-image" class="user-avtar">
-                    </div>
-                    <div class="flex-grow-1 ms-1">
-                      <span class="float-end text-muted">9:10 PM</span>
-                      <p class="text-body mb-1"><b>Cristina Danny </b> invited to join <b> Meeting.</b></p>
-                      <span class="text-muted">Daily scrum meeting time</span>
-                    </div>
-                  </div>
-                </a>
-              </div>
-            </div>
-            <div class="dropdown-divider"></div>
-            <div class="text-center py-2">
-              <a href="#!" class="link-primary">View all</a>
-            </div>
+
+            
           </div>
         </li>
         <li class="dropdown pc-h-item header-user-profile">
@@ -142,10 +76,10 @@
                                             } 
                                             ?>
                                             </h6>
-                  <span>UI/UX Designer</span>
+                                            <!-- <span>UI/UX Designer</span> -->
+                                            <a href="login.php" class="pc-head-link bg-transparent"><i class="ti ti-power text-danger"></i></a>
                 </div>
               </div>
-              <a href="login.php" class="pc-head-link bg-transparent"><i class="ti ti-power text-danger"></i></a>
             </div>
             <ul class="nav drp-tabs nav-fill nav-tabs" id="mydrpTab" role="tablist">
               <li class="nav-item" role="presentation">
